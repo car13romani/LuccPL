bb89995b67c69c8d7e8add0f77225b94317c76ff
Carlos Romani <car13romani@gmail.com>
2018-05-24 14:31:11 -0300

    update functions

--- example/example.R
+++ example/example.R
@@ -1,8 +1,8 @@
 
 
 
-brickpath <- "/home/car/DADOS_MESTRADO/classified_MT_15/bricktest.tif"
-path <- "/home/car/DADOS_MESTRADO/classified_MT_15/recorte2/"
+brickpath <- "/home/carlos/DADOS_MESTRADO/classified_MT_15/bricktest.tif"
+path <- "/home/carlos/DADOS_MESTRADO/classified_MT_15/recorte2/"
 
 # to before, year init = 0; to after, year final = sizets + 1
 query_array <- array(c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,  #relations before,after
@@ -10,6 +10,8 @@ query_array <- array(c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,  #relations before,after
                        4,4,17,17,17,17,17,17,17,17,17,17,17,17, #years final
                        3,1,2,4,5,6,7,8,9,10,11,12,13,14,  #patterns 1,3
                        1,0,1,1,1,1,1,1,1,1,1,1,1,0), c(14,5))  #union_op 1=or, 0=and
+
+
 
 
 dates <- as.character(read.table(paste(path, "/dates.txt", sep=''))$V1)
@@ -43,3 +45,51 @@ LuccPL::plot_output(mtout,dates)
 
 
 
+
+
+
+mt5 <- (mtout[[5]])
+
+fun <- function(x) { x[x>0] <- 1; return(x)}
+
+f <- function(rast){
+  if(rast > 0){
+    rast <- 1
+  }
+  
+}
+
+raster::plot(mt5)
+rrrr <- mtout[[7]]+mtout[[8]]+mtout[[9]]+mtout[[10]]+mtout[[11]]+mtout[[12]]+mtout[[13]]+mtout[[14]]+mtout[[15]]+mtout[[16]]
+rout <- raster::calc(rrrr, fun)
+resu <- mtout[[6]]-rout
+raster::plot(resu)
+
+
+query_array <- array(c(2,3,  #relations before,after
+                       0,7, #years init 4,5
+                       6,17, #years final
+                       3,6,  #patterns 1,3
+                       0,0), c(2,5))  #union_op 1=or, 0=and
+
+query_array <- array(c(2,  #relations before,after
+                       0, #years init 4,5
+                       6, #years final
+                       3,  #patterns 1,3
+                       0), c(1,5))  #union_op 1=or, 0=and
+
+query_array <- array(c(3,  #relations before,after
+                       7, #years init 4,5
+                       17, #years final
+                       6,  #patterns 1,3
+                       0), c(1,5))  #union_op 1=or, 0=and
+
+ts <- c(3,3,4,3,3,3,6,6,4,4,6,6,4,6,6,6)
+blockout <- c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+
+# call Fortran function
+lucc_process <- function(SB, ST, NR, BI, BO, QA) {
+  out <- .Fortran("lucc_process", as.integer(SB), as.integer(ST), as.integer(NR), as.integer(BI), as.integer(BO), as.integer(QA))
+  return(out[[5]])
+}
+lucc_process(SB = 1, ST = 16, NR = 2, BI = ts, BO = blockout, QA = query_array)
--- src/lucc_processF.f
+++ src/lucc_processF.f
@@ -27,7 +27,7 @@ C
 
       SUBROUTINE lucc_process(SB, ST, NR, BI, BO, QA)
 
-      INTEGER SB, ST, NR, I, J, R, L, BI(ST,SB), BO(ST,SB), QA(NR,5), CO, KO
+      INTEGER SB, ST, NR, I, J, R, L, BI(ST,SB), BO(ST,SB), QA(NR,5), CO, KO, TI
 
       CO = 0
       KO = 0
@@ -53,7 +53,7 @@ c            PRINT*, "CO = ", CO
 c            PRINT*, "KO = ", KO
 c            PRINT*, "QAR5 = ", QA(R,5)
 c            PRINT*
-
+c *********************************
 c           AND/OR analisys
             IF (CO.EQ.0) THEN
               IF (KO.EQ.0) THEN
@@ -102,11 +102,289 @@ C           C > 0
                 ENDIF
               ENDIF
             ENDIF
-            
+c *********************************
             
           ENDIF
           
 
+
+
+C     2. MEETS --------------------------------------------
+          IF (QA(R,1).EQ.2) THEN
+            CO = 0
+C     Individual processing
+            IF (BI((QA(R,3)+1),L).NE.(QA(R,4))) THEN
+              IF (BI(QA(R,3),L).EQ.(QA(R,4))) THEN
+                IF (BI(QA(R,3)-1,L).EQ.(QA(R,4))) THEN
+                  BO(QA(R,3),L) = 1
+                  BO((QA(R,3)-1),L) = 1
+                  CO = CO + 2
+                  DO 31 I = (QA(R,3)-2), (QA(R,2)+1), -1
+                    IF (BI(I,L).EQ.QA(R,4)) THEN
+                      BO(I,L) = 1
+                      CO = CO + 1
+                    ELSE
+c                      PRINT*, "MEETS"
+c                      PRINT*, "R = ", R
+c                      PRINT*, "CO = ", CO
+c                      PRINT*, "KO = ", KO
+c                      PRINT*, "QAR5 = ", QA(R,5)
+c                      PRINT*
+                      GOTO 41
+                    ENDIF
+   31             CONTINUE
+                ENDIF
+              ENDIF
+            ENDIF
+           
+
+           
+c *********************************
+c           AND/OR analisys
+          DO 41
+            IF (CO.EQ.0) THEN
+              IF (KO.EQ.0) THEN
+                IF (QA(R,5).EQ.0) THEN
+C                 limpar TS
+c                  PRINT*, "CLEAN"
+                  DO 12 I = 1, ST
+                    BO(I,L) = 0
+   12             CONTINUE
+                  GOTO 50
+                ELSE
+                  KO = 1
+                ENDIF
+C             KO = 1
+              ELSE 
+                IF (KO.EQ.1) THEN
+                  IF (QA(R,5).EQ.0) THEN
+C                  limpar TS
+                    DO 13 I = 1, ST
+                      BO(I,L) = 0
+   13               CONTINUE
+                    GOTO 50
+                  ENDIF
+C               KO = -1
+                ELSE
+                  IF (QA(R,5).EQ.0) THEN
+                    KO = 0
+                  ENDIF
+                ENDIF
+              ENDIF
+
+C           C > 0            
+            ELSE
+c              PRINT*, "KO = ", KO
+c              PRINT*, "CO = ", CO
+c              PRINT*, "QA = ", QA(R,5)
+              IF ((KO.EQ.0).AND.(QA(R,5).EQ.1)) THEN
+                KO = -1
+c                PRINT*, "KO = -1"
+              ELSE 
+                IF (KO.EQ.1) THEN
+                  IF (QA(R,5).EQ.0) THEN
+                    KO = 0
+                  ELSE
+                    KO = -1
+                  ENDIF
+                ELSE
+                  IF (QA(R,5).EQ.0) THEN
+                    KO = 0
+                  ENDIF
+                ENDIF
+              ENDIF
+            ENDIF
+          GOTO 40
+   41   CONTINUE
+c *********************************
+
+
+          ENDIF
+
+C     3. MEEBY -------------------------------------------
+          IF (QA(R,1).EQ.3) THEN
+            CO = 0
+C     Individual processing
+            IF (BI((QA(R,2)-1),L).NE.(QA(R,4))) THEN
+              IF (BI(QA(R,2),L).EQ.(QA(R,4))) THEN
+                IF (BI(QA(R,2)+1,L).EQ.(QA(R,4))) THEN
+                  BO(QA(R,2),L) = 1
+                  BO((QA(R,2)+1),L) = 1
+                  CO = CO + 2
+                  DO 32 I = (QA(R,2)+2), (QA(R,3)-1) 
+                    IF (BI(I,L).EQ.QA(R,4)) THEN
+                      BO(I,L) = 1
+                      CO = CO + 1
+                    ELSE
+c                      PRINT*, "METBY"
+c                      PRINT*, "R = ", R
+c                      PRINT*, "CO = ", CO
+c                      PRINT*, "KO = ", KO
+c                      PRINT*, "QAR5 = ", QA(R,5)
+c                      PRINT*
+                      GOTO 40
+                    ENDIF
+   32             CONTINUE
+                ENDIF
+              ENDIF
+            ENDIF
+
+
+c            PRINT*, "METBY"
+c            PRINT*, "R = ", R
+c            PRINT*, "CO = ", CO
+c            PRINT*, "KO = ", KO
+c            PRINT*, "QAR5 = ", QA(R,5)
+c            PRINT*
+
+
+c *********************************
+c           AND/OR analisys
+            IF (CO.EQ.0) THEN
+              IF (KO.EQ.0) THEN
+                IF (QA(R,5).EQ.0) THEN
+C                 limpar TS
+                  DO 14 I = 1, ST
+                    BO(I,L) = 0
+   14             CONTINUE
+                  GOTO 50
+                ELSE
+                  KO = 1
+                ENDIF
+C             KO = 1
+              ELSE 
+                IF (KO.EQ.1) THEN
+                  IF (QA(R,5).EQ.0) THEN
+C                  limpar TS
+c                    PRINT*, "CLEAN"
+                    DO 15 I = 1, ST
+                      BO(I,L) = 0
+   15               CONTINUE
+                    GOTO 50
+                  ENDIF
+C               KO = -1
+                ELSE
+                  IF (QA(R,5).EQ.0) THEN
+                    KO = 0
+                  ENDIF
+                ENDIF
+              ENDIF
+
+C           C > 0            
+            ELSE
+              IF ((KO.EQ.0).AND.(QA(R,5).EQ.1)) THEN
+                KO = -1
+              ELSE 
+                IF (KO.EQ.1) THEN
+                  IF (QA(R,5).EQ.0) THEN
+                    KO = 0
+                  ELSE
+                    KO = -1
+                  ENDIF
+                ELSE
+                  IF (QA(R,5).EQ.0) THEN
+                    KO = 0
+                  ENDIF
+                ENDIF
+              ENDIF
+            ENDIF
+c *********************************
+
+          ENDIF
+
+
+C     4. HOLDS ----------------------------------
+          IF (QA(R,1).EQ.4) THEN
+            CO = 0
+C     Individual processing
+            IF (BI((QA(R,2)-1),L).NE.(QA(R,4))) THEN
+              IF (BI((QA(R,3)+1),L).NE.(QA(R,4))) THEN
+
+                DO 33 I = (QA(R,2)), (QA(R,3)) 
+                  IF (BI(I,L).EQ.QA(R,4)) THEN
+                    BO(I,L) = 1
+                    CO = CO + 1
+                  ELSE
+                    GOTO 40
+                  ENDIF
+   33           CONTINUE
+                TI = (QA(R,3)-QA(R,2)+1)
+                IF (CO.NE.TI) THEN
+                  DO 110 J = 1, ST
+                    BO(J,L) = 0
+  110            CONTINUE
+                  GOTO 50
+                ENDIF
+              ENDIF
+            ENDIF
+
+c            PRINT*, "R = ", R
+c            PRINT*, "CO = ", CO
+c            PRINT*, "KO = ", KO
+c            PRINT*, "QAR5 = ", QA(R,5)
+c            PRINT*
+c *********************************
+c           AND/OR analisys
+            IF (CO.EQ.0) THEN
+              IF (KO.EQ.0) THEN
+                IF (QA(R,5).EQ.0) THEN
+C                 limpar TS
+                  DO 16 I = 1, ST
+                    BO(I,L) = 0
+   16             CONTINUE
+                  GOTO 50
+                ELSE
+                  KO = 1
+                ENDIF
+C             KO = 1
+              ELSE 
+                IF (KO.EQ.1) THEN
+                  IF (QA(R,5).EQ.0) THEN
+C                  limpar TS
+                    DO 17 I = 1, ST
+                      BO(I,L) = 0
+   17               CONTINUE
+                    GOTO 50
+                  ENDIF
+C               KO = -1
+                ELSE
+                  IF (QA(R,5).EQ.0) THEN
+                    KO = 0
+                  ENDIF
+                ENDIF
+              ENDIF
+
+C           C > 0            
+            ELSE
+              IF ((KO.EQ.0).AND.(QA(R,5).EQ.1)) THEN
+                KO = -1
+              ELSE 
+                IF (KO.EQ.1) THEN
+                  IF (QA(R,5).EQ.0) THEN
+                    KO = 0
+                  ELSE
+                    KO = -1
+                  ENDIF
+                ELSE
+                  IF (QA(R,5).EQ.0) THEN
+                    KO = 0
+                  ENDIF
+                ENDIF
+              ENDIF
+            ENDIF
+c *********************************
+
+          ENDIF
+
    40   CONTINUE
    50 CONTINUE
       END
+
+
+
+
+
+
+
+
+
