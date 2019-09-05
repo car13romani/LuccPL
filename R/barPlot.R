b497239163876d671123e8a6e4fadabeb39da168
Carlos Romani <car13romani@gmail.com>
2019-09-05 00:20:33 -0300

    plot bar and line

--- R/barPlot.R
+++ R/barPlot.R
@@ -38,40 +38,38 @@
 
 
 # df is output of count(for_time_step=TRUE)
-lucc_barplot_result <- function(df=NULL, dates=NULL, style="bar", colors=NULL, path_save_jpeg=NULL){
-  
-  ensurer::ensure_that(df, !is.null(df),
-                       err_desc = "Define a valid array.")
-  ensurer::ensure_that(dates, !is.null(dates),
-                       err_desc = "Define a valid timeline.")
-  ensurer::ensure_that(colors, !is.null(colors),
-                       err_desc = "Define valid colors.")
+lucc_barplot_result <- function(df=NULL, dates=NULL, graph_title=NULL, style="bar", colors=NULL, path_save_jpeg=NULL){
 
-  df$colors1 <- c("0","1")
+  df$colors1 <- c("white","black")
   
   DF1 <- reshape2::melt(df, id.var=c("land_use","colors1"))
   
   colnames(DF1) <- c("LandUse","color","Timeline","Count")
 
-  DF2 <- DF1[order(DF1$color),]
+  #DF2 <- DF1[order(DF1$color),]
   
 
   if(style=="bar"){
-    ggplot(DF1, aes(fill=LandUse, y=Count, x=Timeline)) +
+    ggplot2::ggplot(DF1, aes(fill=LandUse, y=Count, x=Timeline)) +
       geom_bar( stat="identity")+
-      ggtitle("BarPlot")+
+      ggtitle(graph_title)+
+      theme(plot.title = element_text(hjust = 0.5))+
       scale_fill_manual(values=DF1$color)
+    
     if(!is.null(path_save_jpeg)){
-      ggsave(paste0(path_save_jpeg, "/barplotO.jpeg"))
+      ggsave(paste0(path_save_jpeg, "/barplotO.jpeg"), width=10, height=6, dpi=600, device='jpeg')
     }
   }
   if(style=="line"){
-    ggplot2::ggplot(data=DF2, ggplot2::aes(y=Count, x=Timeline, colour=LandUse)) +
-      ggplot2::geom_line()+
-      ggplot2::ggtitle("LinePlot")
+    ggplot2::ggplot(data=DF1, ggplot2::aes(y=Count, x=Timeline, colour=LandUse, group=LandUse)) +
+      #geom_point() +
+      geom_line()+
+      ggtitle(graph_title)+
+      theme(plot.title = element_text(hjust = 0.5))+
+      scale_color_manual(values=DF1$color)
 
     if(!is.null(path_save_jpeg)){
-      ggsave(paste0(path_save_jpeg, "/lineplotO.jpeg"))
+      ggsave(paste0(path_save_jpeg, "/lineplotO.jpeg"), width=10, height=6, dpi=600, device='jpeg')
     }
   }
   
@@ -79,7 +77,7 @@ lucc_barplot_result <- function(df=NULL, dates=NULL, style="bar", colors=NULL, p
 
 
 
-lucc_barplot_data <- function(df=NULL, dates=NULL, style="bar", colors=NULL, path_save_jpeg=NULL){
+lucc_barplot_data <- function(df=NULL, dates=NULL, style="bar", graph_title=NULL, colors=NULL, path_save_jpeg=NULL){
   
  # ensurer::ensure_that(df, !is.null(df),
 #                       err_desc = "Define a valid array.")
@@ -97,26 +95,29 @@ lucc_barplot_data <- function(df=NULL, dates=NULL, style="bar", colors=NULL, pat
     # Stacked
     ggplot(DF1, aes(fill=LandUse, y=Count, x=Timeline)) +
       geom_bar( stat="identity")+
-      ggtitle("BarPlot")+
+      ggtitle(graph_title)+
+      theme(plot.title = element_text(hjust = 0.5))+
       scale_fill_manual(values=DF1$color)
     
     # ggplot(data=dataG, aes(x=dates, y=Count)) +
     #   geom_bar(stat="identity", fill="steelblue")+
     #   geom_text(aes(label=Count), vjust=-0.3, size=3.5)+
     #   ggtitle("BarPlot")+
-    #   theme_minimal()
+    #   
     if(!is.null(path_save_jpeg)){
-      ggsave(paste0(path_save_jpeg, "/barplotI.jpeg"))
+      ggsave(paste0(path_save_jpeg, "/barplotI.jpeg"), width=10, height=6, dpi=600, device='jpeg')
     }
   }
   else{
-    ggplot(data=DF1, aes(fill=LandUse, y=Count, x=Timeline, colour=DF1$color)) +
-      geom_point() +
+    ggplot(data=DF1, aes(fill=LandUse, y=Count, x=Timeline, colour=LandUse, group=LandUse)) +
+      #geom_point() +
       geom_line()+
-      ggtitle("LinePlot")+
-      theme_minimal()
+      ggtitle(graph_title)+
+      theme(plot.title = element_text(hjust = 0.5))+
+      scale_color_manual(values=DF1$color)
+
     if(!is.null(path_save_jpeg)){
-      ggsave(paste0(path_save_jpeg, "/lineplotI.jpeg"))
+      ggsave(paste0(path_save_jpeg, "/lineplotI.jpeg"), width=10, height=6, dpi=600, device='jpeg')
     }
   }
 
