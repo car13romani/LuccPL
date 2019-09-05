6e096e16089756fae1642e843a391c017da8bd3c
Carlos Romani <car13romani@gmail.com>
2019-09-05 00:21:37 -0300

    update demo

--- example/demo.R
+++ example/demo.R
@@ -19,50 +19,44 @@
 # |- ...
 
 
-path <- "/home/carlos/Dropbox/MESTRADO/DADOS_MESTRADO/13_classes_MT_recorte"
+###############  CREATE RASTER BRICK  ###############
+
+path <- "/home/carlos/Dropbox/MESTRADO/DADOS_MESTRADO/13_classes_MT_recorte/"
+# list raster on folder
+lst <- list.files(path=paste0(path,"/raster"),pattern='.tif$',full.names=TRUE)
+# stack creation
+rstack <- raster::stack(lst)
+# brick creation
+rbrick <- raster::brick(rstack,  progress = "text", datatype='INT4S')
+# save raster brick
+raster::writeRaster(rbrick, filename = paste0(path, "/stInput.tif"), datatype='INT4S', overwrite=TRUE, progress = "text")
+
+
+
+
+###############  IMPORT DATA  ###############
 
 dates <- as.character(read.table(paste0(path, "/dates.txt"))$V1)
 colors <- as.character(read.table(paste0(path, "/colors.txt"))$V1)
 metadata <- as.character(read.table(paste0(path, "/metadata.txt"))$V1)
 
-# list files .tif in path
-lst <- list.files(path=paste0(path,"/raster"),pattern='.tif$',full.names=TRUE)
+rbrick <- raster::brick(paste0(path,"/stInput.tif"))
 
-# stack creation
-rstack <- raster::stack(lst)
 
-# brick creation
-rbrick <- raster::brick(rstack,  progress = "text", datatype='INT4S')
-
-#raster::writeRaster(rbrick, filename = paste0(path, "/brickRasterInput.tif"), datatype='INT4S', overwrite=TRUE, progress = "text")
-
-# Using a created brick (disk storage)
-#rbrick <- raster::brick(paste0(path,"/stInput.tif"))
 
 # functions to process
 # "before","after","meets","metby","holds","recur","convert","evolve","count","variance"
 
-# create a list of functions
-# Flist <- c(
-# LuccPL::evolve(pattern_list1 = c("Forest","Savanna"), 
-#        pattern_list2 = c("Pasture_1","Pasture_2"), 
-#        date1 = 2001, 
-#        date2 = 2016, 
-#        dates = dates, 
-#        metadata = metadata),
-# "or",
-# LuccPL::convert(pattern_list1 = c("Forest","Savanna"), 
-#         pattern_list2 = c("Pasture_1","Pasture_2"), 
-#         date1 = 2001, 
-#         date2 = 2016, 
-#         dates = dates, 
-#         metadata = metadata)
-# )
 
+###############  PROCESS  ###############
 
 Flist <- c(
   LuccPL::evolve(pattern_list1 = c("03.Forest"), 
-                 pattern_list2 = c("04.Pasture"), 
+                 pattern_list2 = c("05.Soy_Corn",
+                                   "06.Soy_Cotton",
+                                   "07.Soy_Fallow",
+                                   "08.Soy_Millet",
+                                   "09.Soy_Sunflower"),
                  date1 = 2001, 
                  date2 = 2017, 
                  dates = dates, 
@@ -76,36 +70,51 @@ query_array <- LuccPL::query(rbrick,Flist)
 # Function to process a query array in rasterBrick
 brickRasterOutput <- LuccPL::event(rbrick, query_array)
 
+
 # save raster result on disk
-#raster::writeRaster(brickRasterOutput, filename = paste0(path, "/brickRasterOutput.tif"), datatype='INT4S', overwrite=TRUE, progress = "text")
+raster::writeRaster(brickRasterOutput, filename = paste0(path, "/stOutput.tif"), datatype='INT4S', overwrite=TRUE, progress = "text")
 
 
-#for (i in 1:nlayers(brickRasterOutput)) {
-#  raster::writeRaster(brickRasterOutput[[i]], filename = paste0(path, "/raster_out/raster_out_",i,".tif"), datatype='INT4S', overwrite=TRUE, progress = "text")
-#}
+for (i in 1:nlayers(brickRasterOutput)) {
+  raster::writeRaster(brickRasterOutput[[i]], filename = paste0(path, "/raster_out/raster_out_",i,".tif"), datatype='INT4S', overwrite=TRUE, progress = "text")
+}
 
 
-#rastervar <- variance(rbrick)
-#count_output <- count(brickRasterOutput)
+###############  COUNT  ###############
 
-#f <- hist(count_output)
+count_output <- LuccPL::count(brickRasterOutput)
 
-
-graphIn <- count(rbrick, for_time_step = TRUE, metadata = metadata, dates = dates)
-graphOut <- count(brickRasterOutput, for_time_step = TRUE, metadata = c("0.false","1.true"), dates = dates)
-
-lucc_barplot_data(df = graphIn, dates = dates, style="bar", colors = colors, path_save_jpeg = path)
-lucc_barplot_result(df = graphOut, dates = dates, style="bar", colors = c("white","black"), path_save_jpeg = path)
-
-
-lucc_barplot_data(df = graphIn, dates = dates, style="line", colors = colors, path_save_jpeg = path)
-lucc_barplot_result(df = graphOut, dates = dates, style="line", colors = c("white","black"), path_save_jpeg = path)
+raster::writeRaster(count_output, filename = paste0(path, "/stCount.tif"), datatype='INT4S', overwrite=TRUE, progress = "text")
 
 
 
 
-graph
-#raster::plot(count_output)
+
+###############  PLOT  ###############
+
+# plot input
+LuccPL::plot_input(rbrick,dates,metadata,colors,paste0(path,"/mpInput.jpeg"), map_title="13 classes MT recorte", Width = 1200, Height = 1200)
+
+# plot output
+LuccPL::plot_output(brickRasterOutput,dates,paste0(path,"/mpOutput.jpeg"), map_title="Evolve Forest - Soy", Width = 1200, Height = 1200)
+
+# create table 
+graphIn <- LuccPL::count(rbrick, for_time_step = TRUE, metadata = metadata, dates = dates)
+graphOut <- LuccPL::count(brickRasterOutput, for_time_step = TRUE, metadata = c("0.false","1.true"), dates = dates)
+
+# export csv
+write.csv(graphIn, paste0(path,"/graphIn.csv"))
+write.csv(graphOut, paste0(path,"/graphOut.csv"))
+
+# export jpeg bar graph
+LuccPL::lucc_barplot_data(df = graphIn, dates = dates, graph_title="13 classes MT recorte", style="bar", colors = colors, path_save_jpeg = path)
+LuccPL::lucc_barplot_result(df = graphOut, dates = dates, graph_title="Evolve Forest - Soy", style="bar", colors = c("white","black"), path_save_jpeg = path)
+
+# export line graph
+LuccPL::lucc_barplot_data(df = graphIn, dates = dates, graph_title="13 classes MT recorte", style="line", colors = colors, path_save_jpeg = path)
+LuccPL::lucc_barplot_result(df = graphOut, dates = dates, graph_title="Evolve Forest - Soy", style="line", colors = c("white","black"), path_save_jpeg = path)
+
+
 
 
 
